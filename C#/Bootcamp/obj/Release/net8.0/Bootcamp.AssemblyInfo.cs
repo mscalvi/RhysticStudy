f2bad0fc6c9f1d260dc660d7fd9a18b5ade9e320
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2024-04-10 23:39:32 -0300

    Muita coisa

--- C#/Bootcamp/obj/Release/net8.0/Bootcamp.AssemblyInfo.cs
+++ C#/Bootcamp/obj/Release/net8.0/Bootcamp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Bootcamp")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57789652767d1cbe6f90d036c3d516090e7237f3")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
+[assembly: System.Reflection.AssemblyProductAttribute("Bootcamp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Bootcamp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- C#/ConsoleApp2/obj/Release/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ C#/ConsoleApp2/obj/Release/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57789652767d1cbe6f90d036c3d516090e7237f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
