ceb06b93ba1be6a5ae290328ee98bce1d6998b11
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2024-04-17 12:58:23 -0300

    Bootcamp

---
+++ C#/Bootcamp/Bootcamp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Bootcamp.Common.Models;
+
+Pessoa pessoa1 = new Pessoa();
+Pessoa pessoa2 = new Pessoa();
+
+pessoa1.Nome = "Carlos";
+pessoa1.Idade = 28;
+pessoa1.Apresentar();
+
+pessoa2.Nome = "Cláudio";
+pessoa2.Idade = 26;
+pessoa2.Apresentar();
+
+Console.WriteLine(DateTime.Now.ToString());
