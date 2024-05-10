e2dba9f5ee32c4fa1634f025b7b8cc8481ca2980
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2024-05-10 13:42:50 -0300

    Bootcamp2

--- C#/Bootcamp/Bootcamp.Common/obj/Debug/net8.0/Bootcamp.Common.AssemblyInfo.cs
+++ C#/Bootcamp/Bootcamp.Common/obj/Debug/net8.0/Bootcamp.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bootcamp.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46755baa969986bd86189485e72753f84bbff76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f96acdd2fa32a052b9dca32b7a100f8e1ec6c575")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bootcamp.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bootcamp.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
