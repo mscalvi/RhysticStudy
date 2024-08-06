ce80af42c081fcc57bdba8b383d916929032b48b
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2024-08-06 16:48:53 -0300

    Classe Posição

---
+++ C#/Xadrez/Xadrez/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+        }
+
+    }
+}
